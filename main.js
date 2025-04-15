63bbbf5b3fb547b1b5b4d564ab1d3edecad7f705
mahmudulashev <urgutmahmud@gmail.com>
2025-04-15 20:46:07 +0500

    Implement off-screen hamburger menu: Add functionality and styling for a responsive navigation menu, enhancing user experience on mobile devices.

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 
+const hamMenu = document.querySelector('.ham-menu');
+const offScreen = document.querySelector('.off-screen-menu');
+const posterColumns = document.querySelectorAll('.poster-column'); // Posterlar ustunlari
+
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');     
+    offScreen.classList.toggle('active');
+
+    // Posterlarni yashirish yoki ko'rsatish
+    posterColumns.forEach(column => {
+        if (offScreen.classList.contains('active')) {
+            column.style.visibility = 'hidden'; // Posterlarni yashirish
+            column.style.zIndex = '-1'; // Posterlarni menyudan orqaga o'tkazish
+        } else {
+            column.style.visibility = 'visible'; // Posterlarni qayta ko'rsatish
+            column.style.zIndex = '0'; // Posterlarni asl holatiga qaytarish
+        }
+    });
+});
