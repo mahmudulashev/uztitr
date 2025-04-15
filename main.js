390fb812aa75fd2bedab951148ba891959426d12
mahmudulashev <urgutmahmud@gmail.com>
2025-04-15 23:22:49 +0500

    Refactor styling and layout: Update button styles, enhance responsiveness, and improve rating display logic for better user experience.

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 
 const hamMenu = document.querySelector('.ham-menu');
 const offScreen = document.querySelector('.off-screen-menu');
-const posterColumns = document.querySelectorAll('.poster-column'); // Posterlar ustunlari
+const posterColumns = document.querySelectorAll('.poster-column'); 
 
 
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');     
     offScreen.classList.toggle('active');
 
-    // Posterlarni yashirish yoki ko'rsatish
+    
     posterColumns.forEach(column => {
         if (offScreen.classList.contains('active')) {
-            column.style.visibility = 'hidden'; // Posterlarni yashirish
-            column.style.zIndex = '-1'; // Posterlarni menyudan orqaga o'tkazish
+            column.style.visibility = 'hidden'; 
+            column.style.zIndex = '-1'; 
         } else {
-            column.style.visibility = 'visible'; // Posterlarni qayta ko'rsatish
-            column.style.zIndex = '0'; // Posterlarni asl holatiga qaytarish
+            column.style.visibility = 'visible'; 
+            column.style.zIndex = '0'; 
         }
     });
 });
